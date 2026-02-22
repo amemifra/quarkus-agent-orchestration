4d99d8ce752ad40ad51381dff596341bd477bbbc
Francesco <francesco@pyxcloud.info>
2026-02-22 01:59:04 +0100

    chore: add .gitignore, remove tracked build artifacts

--- deployment/target/generated-sources/annotations/io/quarkiverse/agent/deployment/AgentExtensionProcessor$$accessor.java
+++
@@ -1,8 +0,0 @@
-package io.quarkiverse.agent.deployment;
-@io.quarkus.Generated("Quarkus annotation processor")
-public final class AgentExtensionProcessor$$accessor {
-    private AgentExtensionProcessor$$accessor() {}
-    public static Object construct() {
-        return new AgentExtensionProcessor();
-    }
-}
